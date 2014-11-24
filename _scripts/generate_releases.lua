620169cc5262d879e568b2eb869b2046fe71f637
Natanael Copa <ncopa@alpinelinux.org>
2014-11-24 13:40:06 +0100

    generate_releases: add download urls for iso and sha sums
    and add a default release

--- _scripts/generate_releases.lua
+++ _scripts/generate_releases.lua
@@ -2,13 +2,19 @@
 
 yaml = require("yaml")
 
+url_prefix="http://wiki.alpinelinux.org/cgi-bin/dl.cgi"
 t = {}
 
 for i = 1,#arg do
 	local f = assert(io.open(arg[i]))
 
 	for _,v in pairs(yaml.load(f:read("*a"))) do
+		v.iso_url = ("%s/%s/releases/%s/%s"):format(url_prefix,
+			v.branch, v.arch, v.iso)
+		v.sha256_url = ("%s.sha256"):format(v.iso_url)
+		v.sha1_url = ("%s.sha256"):format(v.iso_url)
 		v.size_mb=math.floor(v.size/(1024*1024))
+
 		local flavor = t[v.flavor]
 		if flavor == nil then
 			flavor = { archs = {}}
@@ -18,5 +24,8 @@ for i = 1,#arg do
 		t[v.flavor] = flavor
 	end
 end
+
+-- default release
+t.default = t.alpine.x86_64
 
 io.write(yaml.dump(t))
